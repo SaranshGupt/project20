1ddc1ca5c97a2eb50e5d02b867db89fbd6a3bb18
SaranshGupt <72692338+SaranshGupt@users.noreply.github.com>
2020-11-03 22:56:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var car,wall;
+var speed, weight; 
+
+
+function setup() {
+  createCanvas(1600, 400);
+
+// define speed and weight in random
+
+
+//create a car give it a velocity and color
+
+
+
+//create a wall and give color
+
+
+}
+
+
+function draw() {
+  background(0);
+//When collision happens, based on deformation calculations determine the color of the
+//car as red, yellow or green.
+//refer to the document
+
+  }  
+  
+  drawSprites();
+ 
+}
+
+
